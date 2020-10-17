76dde333112abd38e37ea1c48aff42ce3d34a7e5
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-10-16 23:04:43 -0400

    initial commit

---
+++ Interactive.pas
@@ -0,0 +1,9 @@
+program Interactive (input, output);
+    {Demonstrates interactive program input and output.}
+var Number: integer;
+begin
+    write ('Please enter an integer value. ');
+    readln (Number);
+    write ('The number you entered was: ');
+    writeln (Number)    {Print the value of Number.}
+end.
