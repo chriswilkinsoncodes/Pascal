79d81c68f42b258dfa906d4d3dc5486d9d72b6bc
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-10-17 17:19:24 -0400

    initial commit

---
+++ EasyCalculations.pas
@@ -0,0 +1,11 @@
+{Oh! Pascal! p. 19}
+program EasyCalculations (input, output);
+    {Shows that expressions are evaluated before output.}
+var Cat, Rat: integer;
+begin
+    writeln ('Enter two integer values.');
+    readln (Cat, Rat);
+    writeln (Cat, Rat);
+    writeln (Cat+Rat, Cat-Rat);
+    writeln (Cat/Rat, Cat*Rat);
+end.
