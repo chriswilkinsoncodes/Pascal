42ff35a816b92ad9c258e20566be2c6860009c21
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-10-17 16:36:00 -0400

    added Oh! Pascal! page nums

--- FirstRun.pas
+++ FirstRun.pas
@@ -1,3 +1,4 @@
+{Oh! Pascal! p. 4}
 program FirstRun (output);
     {This is our first program.}
 begin
--- Interactive.pas
+++ Interactive.pas
@@ -1,3 +1,4 @@
+{Oh! Pascal! p. 14}
 program Interactive (input, output);
     {Demonstrates interactive program input and output.}
 var Number: integer;
