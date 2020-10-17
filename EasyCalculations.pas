1dff0898927c8b7a57200e354f9554d9e61674b6
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-10-17 17:29:11 -0400

    added field widths

--- EasyCalculations.pas
+++ EasyCalculations.pas
@@ -5,7 +5,7 @@ var Cat, Rat: integer;
 begin
     writeln ('Enter two integer values.');
     readln (Cat, Rat);
-    writeln (Cat, Rat);
-    writeln (Cat+Rat, Cat-Rat);
-    writeln (Cat/Rat, Cat*Rat);
+    writeln (Cat:3, Rat:3);
+    writeln (Cat+Rat:5, Cat-Rat:5);
+    writeln (Cat/Rat:5, Cat*Rat:5);
 end.
