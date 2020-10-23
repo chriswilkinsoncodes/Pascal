e577dc637cb4204d8ad68fcd8d6c1dc63a0c13f5
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-10-23 08:58:40 -0400

    initial commit

---
+++ Music.pas
@@ -0,0 +1,19 @@
+{Oh! Pascal! p. 75}
+program Music (output);
+    {Illustrates scope of identifiers.}
+const SCALE = 'Base clef '; {This is a text constant.}
+var JohnnyOneNote: char;
+procedure Tune; {Note the identically named local identifiers.}
+    const SCALE = 'Treble clef ';
+    var JohnnyOneNote: char;
+    begin
+        JohnnyOneNote := 'A';
+        writeln (SCALE, JohnnyOneNote);        
+    end; {Tune}
+begin
+    JohnnyOneNote := 'D
+    ';
+    writeln (SCALE, JohnnyOneNote);
+    Tune;
+    writeln (SCALE, JohnnyOneNote);
+end. {Music}
