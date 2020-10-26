4c8cfd8364072c12a7e17ae67c921cc28115bfa3
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2020-10-26 15:12:08 -0400

    added FindSquareSum procedure

--- Exchanges.pas
+++ Exchanges.pas
@@ -1,7 +1,7 @@
 {Oh! Pascal! p. 78}
 program Exchanges (output);
     {Demonstrates the use of variable parameters.}
-var Little, Big: integer;
+var Little, Big, Answer: integer;
 procedure Switch (var First, Second: integer);
     {Exchanges the values of First and Second.}
     var Temporary: integer;
@@ -10,10 +10,23 @@ procedure Switch (var First, Second: integer);
         First := Second;
         Second := Temporary
     end; {Switch}
+procedure FindSquareSum (First, Second: integer; var Sum: integer);
+    {Uses value parameters to help compute a variable parameter.}
+    begin
+        First := sqr (First);
+        Second := sqr (Second);
+        Sum := First + Second
+    end; {FindSquareSum}
 begin
     Big := 5;
     Little := 10;
     writeln (Big, Little);
     Switch (Big, Little);
-    writeln (Big, Little)
+    writeln (Big, Little);
+    FindSquareSum (5, 9, Answer);
+    writeln (Answer);
+    FindSquareSum (3 + 2, 9 mod 8, Answer);
+    writeln (Answer);
+    FindSquareSum (Big, Little, Answer);
+    writeln (Big, Little, Answer)
 end. {Exchanges}
