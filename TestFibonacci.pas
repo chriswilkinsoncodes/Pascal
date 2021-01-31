4a2343d3a0b1d7fd18853cba0e24b10dc4e2f0eb
chriswilkinsoncodes <chriswilkinsoncodes@gmail.com>
2021-01-30 20:18:09 -0500

    - added Oh! Pascal! page comment - removed dupliate end - added space around output numbers
    - result for 25 is greater than MAXINT for some compilers and output may display and error

--- TestFibonacci.pas
+++ TestFibonacci.pas
@@ -1,3 +1,4 @@
+{Oh! Pascal! p. 239}
 program TestFibonacci (input, output);
     {Test a function that recursively generates Fibonacci numbers.}
 
@@ -9,12 +10,11 @@ function Fibonacci(Which: integer): integer;
             then Fibonacci := 1
             else Fibonacci := Fibonacci(Which-1) + Fibonacci(Which-2)
         end; {Fibonacci}
-    end;
 
 begin
     writeln ('Reading four test entries.');
     readln (Test1, Test2, Test3, Test4);
     writeln ('Fibonacci numbers', Test1:4, Test2:4, Test3:4, Test4:4, ' are:');
-    write (Fibonacci(Test1), Fibonacci(Test2));
-    writeln (Fibonacci(Test3), Fibonacci(Test4))
+    write (Fibonacci(Test1):6, Fibonacci(Test2):6);
+    writeln (Fibonacci(Test3):6, Fibonacci(Test4):6)
 end. {Fibonacci}
